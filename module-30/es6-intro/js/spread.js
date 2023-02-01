9848a10647e453cbc046a4bf7f186ebc4a4c41c2
Tareq35 <a.t.tariqulislam@gmail.com>
2023-02-01 09:04:25 +0600

    code updated

---
+++ module-30/es6-intro/js/spread.js
@@ -0,0 +1,6 @@
+const max = Math.max(12, 85, 999, 78);
+// console.log(max);
+const numbers = [12, 85, 999, 78];
+const largest = Math.max(...numbers);
+// console.log(...numbers);
+// console.log(largest);
